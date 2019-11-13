b1ca3709e50ee4c7e0418d5fe0a6093ba7efb79d
appleEmily <emiri3128@gmail.com>
2019-11-13 13:38:14 +0900

    Initial Commit

---
+++ sound_Nov/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sound_Nov
+//
+//  Created by 野崎絵未里 on 2019/11/13.
+//  Copyright © 2019 emily.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
